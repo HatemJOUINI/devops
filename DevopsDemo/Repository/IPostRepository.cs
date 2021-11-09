79c5e46e9844b38a821e6bcab1817a4efcdca78e
hatem <a1@a.a>
2021-11-09 08:26:04 +0100

    first commit

---
+++ DevopsDemo/Repository/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using DevopsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevopsDemo.Repository
+{
+     public  interface IPostRepository
+    {
+        List<PostViewModel> GetPosts();
+    }
+}
